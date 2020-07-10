d80532bc69c042c6fed7a6975d5ab4a9c38ee1c7
Kevin Li <kevinli4321@gmail.com>
2020-07-09 21:59:18 -0400

    Initial Commit

---
+++ Shared/UnwoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnwoodApp.swift
+//  Shared
+//
+//  Created by Kevin Li on 7/9/20.
+//
+
+import SwiftUI
+
+@main
+struct UnwoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
